339a59ab25a950f33797043d9a7323db71174a9f
heena3443 <61155560+heena3443@users.noreply.github.com>
2020-09-24 15:51:39 -0500

    Add files via upload

---
+++ Lab 5/src/TravelDestination.java
@@ -0,0 +1,8 @@
+
+public class TravelDestination {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
+	}
+
+}
