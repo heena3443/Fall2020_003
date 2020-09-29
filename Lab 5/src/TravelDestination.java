af2124f06388f19f00a19729ed76770ac8fa4f86
JRamoz <55769661+JRamoz@users.noreply.github.com>
2020-09-29 13:19:05 -0500

    Update TravelDestination.java
    This is my print statement for lab5

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -3,6 +3,7 @@ public class TravelDestination {
 
 	public static void main(String[] args) {
 		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
+		System.out.println("My name is Julie Ramoz and I love to draw.");
 	}
 
 }
