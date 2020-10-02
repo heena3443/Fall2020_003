4155f8201227eee13e6cf6180c42be3164011f60
TannerJMcLees <47405174+TannerJMcLees@users.noreply.github.com>
2020-10-01 20:13:09 -0500

    Added print statement
    Added a new print statement with my name and one of my hobbies

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -3,6 +3,9 @@ public class TravelDestination {
 
 	public static void main(String[] args) {
 		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
+		
+		//Adding a print statement with name and hobby
+		System.out.println("My name is Tanner McLees. One of my hobbies is skateboarding.");
 	}
 
 }
