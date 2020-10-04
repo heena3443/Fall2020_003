6b50f804d441c9fbf07c1c9a162fcc1b429de5fe
BethanySalazar <bethanysalazar2007@hotmail.com>
2020-10-04 16:03:14 -0500

    Update TravelDestination.java
    Added a print statement to the bottom of the main method. Includes full name and one of my hobbies.

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -3,6 +3,7 @@ public class TravelDestination {
 
 	public static void main(String[] args) {
 		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
+		System.out.println("My name is Bethany Salazar. I love exploring different trails around my apartment complex.");
 	}
 
 }
