dc760c8d3007990d2f8d27c9f2153a1a56b22ca9
BethanySalazar <bethanysalazar2007@hotmail.com>
2020-10-04 19:10:55 -0500

    Update TravelDestination.java

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -3,7 +3,10 @@ public class TravelDestination {
 
 	public static void main(String[] args) {
 		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
-		System.out.println("My name is Bethany Salazar. I love exploring different trails around my apartment complex.");
+		
+		//Print statement with full name and hobby
+		System.out.println("My name is Bethany Salazar. I love exploring different trails around my apartment complex with my puppy.");
+		
 	}
 
 }
