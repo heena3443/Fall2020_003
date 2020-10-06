4994dec2a54ddb98078f56799325aad05e2c6385
jmo184 <jmo184@my.utsa.edu>
2020-10-06 15:11:27 -0500

    First commit

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -87,6 +87,10 @@ public class TravelDestination {
 		System.out.println("My name is Samir Stanislav Yezhnikovsky and I like to read.");
 		
 		System.out.println("My name is Amber Beserra, one of my hobbies is drawing");
+		
+		//Task 2: Print name and hobby
+		System.out.println("My name is Jackie Tran. My hobby is watching and playing basketball.");
+		
 	}
 
 }
