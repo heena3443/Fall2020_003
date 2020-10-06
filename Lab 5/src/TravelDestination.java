ad5b4de0420cbe14e38d6d8070624873694bcdf4
JRamoz <55769661+JRamoz@users.noreply.github.com>
2020-10-05 23:34:39 -0500

    Update TravelDestination.java
    T his is my commit change for lab 5 task 2.

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -4,6 +4,9 @@ public class TravelDestination {
 	public static void main(String[] args) {
 		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
 		
+		//Adding print statement for name and hobby
+		System.out.println("My name is Julie Ramoz. I enjoy creating digital art in my freetime. :)");
+		
 		//Adding a print statement with name and hobby
 		System.out.println("My name is Tanner McLees. One of my hobbies is skateboarding.");
 		
