6ae3e97bbdf3907c5f4abba78cc86b0542f993bf
Miguel Alejandro <71030202+elmondayuca@users.noreply.github.com>
2020-10-06 23:05:51 -0500

    Completed task 2 and added a print statement

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -40,6 +40,9 @@ public class TravelDestination {
 		//Print statement with full name and hobby
 		System.out.println("My name is Michael Stepp and I like to play electric guitar");
 		
+		//Print statement with full name and hobby
+		System.out.println("My name is Miguel A. Avilés Bonilla. I like to watch pixar movies.\n");
+		
 	}
 
 }
